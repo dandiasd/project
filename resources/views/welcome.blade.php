d8c19e19edad489a73bcc14f61e55a46326cf03b
IndraSujadi <indrasujadi0@gmail.com>
2020-06-18 15:24:53 +0700

    Test
    INdra

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Indra
                 </div>
 
                 <div class="links">
